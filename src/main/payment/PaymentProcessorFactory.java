74a00caee62bf96d03c236832fa16b0347ecdc49
julianbarrycodes <contact.julian.barry@gmail.com>
2025-02-11 09:40:43 +0100

    Refactoring the main

--- src/main/payment/PaymentProcessorFactory.java
+++ src/main/payment/PaymentProcessorFactory.java
@@ -12,13 +12,11 @@ public class PaymentProcessorFactory {
      *
      * @param method the {@link PaymentMethod} for which the processor is to be created.
      * @return the appropriate {@link PaymentProcessor} instance.
-     * @throws IllegalArgumentException if the {@code method} is unsupported.
      */
     public static PaymentProcessor createProcessor(@NotNull PaymentMethod method) {
         return switch (method) {
             case CREDIT_CARD -> new CreditCardProcessor();
             case MOBILE_WALLET -> new MobileWalletProcessor();
-            default -> throw new IllegalArgumentException("Unsupported payment method: " + method);
         };
     }
 }
